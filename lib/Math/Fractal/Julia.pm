4a4a81811593ce54fbe012fdf2038910be45ae98
Jeffrey T. Palmer <jtpalmer@cpan.org>
2012-06-23 23:38:58 +0000

    Import of JTPALMER/Math-Fractal-Julia-0.000003 from CPAN.
    gitpan-cpan-distribution: Math-Fractal-Julia
    gitpan-cpan-version:      0.000003
    gitpan-cpan-path:         JTPALMER/Math-Fractal-Julia-0.000003.tar.gz
    gitpan-cpan-author:       JTPALMER
    gitpan-cpan-maturity:     released

--- lib/Math/Fractal/Julia.pm
+++ lib/Math/Fractal/Julia.pm
@@ -1,6 +1,6 @@
 package Math::Fractal::Julia;
 {
-  $Math::Fractal::Julia::VERSION = '0.000002'; # TRIAL
+  $Math::Fractal::Julia::VERSION = '0.000003';
 }
 use strict;
 use warnings;
@@ -40,7 +40,7 @@ Math::Fractal::Julia - Calculate points in the Julia set
 
 =head1 VERSION
 
-version 0.000002
+version 0.000003
 
 =head1 SYNOPSIS
 
@@ -89,25 +89,40 @@ L<Math::Fractal::Mandelbrot>.
 
 =back
 
-Options:
+Creates a new Math::Fractal::Object.  If no options are provided, the
+default values will be used.
 
 =over 4
 
-=item * max_iter => $iters
+=item options
 
-=item * limit => $limit
+The C<options> hash may contain any or all of the following:
 
-=item * bounds => [ $x1, $x2, $y1, $y2, $width, $height ]
+max_iter => $iters
 
-=item * constant => [ $cx, $cy ]
+limit => $limit
+
+bounds => [ $x1, $x2, $y1, $y2, $width, $height ]
+
+constant => [ $cx, $cy ]
 
 =back
 
+The default maximum number of iterations is 600.
+The default limit is 5.
+The default bounds is [-2.2, -1.1, 1.0, 1.1, 640, 480].
+The default constant is [0.0, 0.0].
+
     my $julia = Math::Fractal::Julia->new();
+
     my $julia = Math::Fractal::Julia->new(%options);
 
-Creates a new Math::Fractal::Object.  If no options are provided, the
-default values will be used.
+    my $julia = Math::Fractal::Julia->new(
+        max_iter => $iters,
+        limit    => $limit,
+        bounds   => [ $x1, $x2, $y1, $y2, $width, $height ],
+        constant => [ $cx, $cy ],
+    );
 
 =head2 set_max_iter
 
@@ -115,12 +130,15 @@ default values will be used.
 
 =item Arguments: $max
 
+=item Return value: undefined
+
 =back
 
-    Math::Fractal::Julia->set_max_iter($max);
-    $julia->set_max_iter($max);
-
 Set the maximum number of iterations.  The default value is 600.
+
+    Math::Fractal::Julia->set_max_iter($max);
+
+    $julia->set_max_iter($max);
 
 =head2 set_limit
 
@@ -128,12 +146,15 @@ Set the maximum number of iterations.  The default value is 600.
 
 =item Arguments: $limit
 
+=item Return value: undefined
+
 =back
 
-    Math::Fractal::Julia->set_limit($limit);
-    $julia->set_limit($limit);
-
 The default value is 5.
+
+    Math::Fractal::Julia->set_limit($limit);
+
+    $julia->set_limit($limit);
 
 =head2 set_bounds
 
@@ -141,15 +162,19 @@ The default value is 5.
 
 =item Arguments: $x1, $y1, $x2, $y2, $width, $height
 
-=back
+=item Return value: undefined
 
-    Math::Fractal::Julia->set_bounds( $x1, $y1, $x2, $y2, $width, $height );
-    $julia->set_bounds( $x1, $x2, $y1, $y2, $width, $height );
+=back
 
 Set the bounds of the set.  The first four values (C<$x1>, C<$y1>,
 C<$x2>, C<$y2>) define a rectangle.  The remaining two (C<$width>,
 C<$height>) define a viewport.
+
 The default values are (-2.2, -1.1, 1.0, 1.1, 640, 480).
+
+    Math::Fractal::Julia->set_bounds( $x1, $y1, $x2, $y2, $width, $height );
+
+    $julia->set_bounds( $x1, $x2, $y1, $y2, $width, $height );
 
 =head2 set_constant
 
@@ -157,12 +182,15 @@ The default values are (-2.2, -1.1, 1.0, 1.1, 640, 480).
 
 =item Arguments: $cx, $cy
 
+=item Return value: undefined
+
 =back
 
-    Math::Fractal::Julia->set_constant( $cx, $cy );
-    $julia->set_constant( $cx, $cy );
-
 The default values are (0.0, 0.0).
+
+    Math::Fractal::Julia->set_constant( $cx, $cy );
+
+    $julia->set_constant( $cx, $cy );
 
 =head2 point
 
@@ -175,13 +203,14 @@ or 0 if the the limit is not exceeded.
 
 =back
 
-    $iter = Math::Fractal::Julia->point( $x, $y );
-    $iter = $julia->point( $x, $y );
-
 This function translates the coordinates using the bounds and then
 iterates.
 
-=head1 BUGS AND LIMITATIONS
+    $iters = Math::Fractal::Julia->point( $x, $y );
+
+    $iters = $julia->point( $x, $y );
+
+=head1 CAVEATS
 
 =over 4
 
@@ -192,7 +221,7 @@ This module is not thread-safe.
 =item *
 
 Any packages derived from C<Math::Fractal::Julia> will share the same
-internal state when used procedurally.
+internal state when the procedural interface is used.
 
 =back
 
@@ -202,7 +231,7 @@ internal state when used procedurally.
 
 =item * L<Math::Fractal::Mandelbrot>
 
-=item * L<http://en.wikipedia.org/wiki/Julia_set>
+=item * L<Julia set|http://en.wikipedia.org/wiki/Julia_set>
 
 =back
 
